db22327b92d1ddeda17413e0e69e359a7bd50632
damienbod <damien_bod@hotmail.com>
2019-04-21 14:03:59 +0200

    init topic messaging

---
+++ ServiceBusMessaging/ServiceBusTopicSender.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusMessaging
+{
+    public class ServiceBusTopicSender
+    {
+        private readonly TopicClient _topicClient;
+        private readonly IConfiguration _configuration;
+        private const string TOPIC_PATH = "https://damienbod-service-bus.servicebus.windows.net/mytopic";
+
+        public ServiceBusTopicSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _topicClient = new TopicClient(
+                _configuration.GetConnectionString("ServiceBusConnectionString"), 
+                TOPIC_PATH
+            );
+        }
+        
+        public async Task SendMessage(MyPayload payload)
+        {
+            string data = JsonConvert.SerializeObject(payload);
+            Message message = new Message(Encoding.UTF8.GetBytes(data));
+
+            await _topicClient.SendAsync(message);
+        }
+        
+    }
+}
