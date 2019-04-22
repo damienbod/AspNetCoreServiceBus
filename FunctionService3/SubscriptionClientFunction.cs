54a620bc531c4a045bfd1cae3cc022ba89bcc38e
damienbod <damien_bod@hotmail.com>
2019-04-22 23:29:44 +0200

    Adding a subscription for the topic

--- FunctionService3/SubscriptionClientFunction.cs
+++ FunctionService3/SubscriptionClientFunction.cs
@@ -6,8 +6,8 @@ namespace FunctionService3
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("mytopic", "mysubscription", Connection = "MyConn")]string mySbMsg, ILogger log)
+        [FunctionName("SubscriptionClientFunction")]
+        public static void Run([ServiceBusTrigger("mytopic", "functionsubscription", Connection = "ServiceBusConnectionString")]string mySbMsg, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
         }
