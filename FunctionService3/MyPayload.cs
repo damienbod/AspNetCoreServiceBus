a249251f05767f4c516950ed3935b5245a985cfb
damienbod <damien_bod@hotmail.com>
2019-04-23 17:50:40 +0200

    consuming the topic subscription in a serverless function

---
+++ FunctionService3/MyPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace FunctionService3
+{
+    public class MyPayload
+    {
+        public string Name { get; set; }
+
+        public int Goals { get; set; }
+
+        public bool Delete { get; set; }
+    }
+}
--- FunctionService3/SubscriptionClientFunction.cs
+++ FunctionService3/SubscriptionClientFunction.cs
@@ -1,15 +1,20 @@
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace FunctionService3
 {
     public static class Function1
     {
         [FunctionName("SubscriptionClientFunction")]
-        public static void Run([ServiceBusTrigger("mytopic", "functionsubscription", Connection = "ServiceBusConnectionString")]string mySbMsg, ILogger log)
+        public static void Run([ServiceBusTrigger(
+            "mytopic", 
+            "functionsubscription",
+            Connection = "ServiceBusConnectionString")]string mySbMsg, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+
+            var payload = JsonConvert.DeserializeObject<MyPayload>(mySbMsg);
         }
     }
 }
