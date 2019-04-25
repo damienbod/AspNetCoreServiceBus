6f2ddb6bcbc06075950ffab419dbfa0a56a72e17
damienbod <damien_bod@hotmail.com>
2019-04-25 18:37:19 +0200

    Adding the database migrations

---
+++ AspNetCoreServiceBusApi2/Migrations/20190425163555_init_db.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreServiceBusApi2.Migrations
+{
+    public partial class init_db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payloads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Goals = table.Column<int>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payloads", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payloads");
+        }
+    }
+}
