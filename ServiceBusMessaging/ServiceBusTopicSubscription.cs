05acc62ff5c26f94c0e48f78e6f558e178dc0049
damienbod <damien_bod@hotmail.com>
2021-06-18 15:05:16 +0200

    clean up

--- ServiceBusMessaging/ServiceBusTopicSubscription.cs
+++ ServiceBusMessaging/ServiceBusTopicSubscription.cs
@@ -9,13 +9,6 @@ using System.Threading.Tasks;
 
 namespace ServiceBusMessaging
 {
-    public interface IServiceBusTopicSubscription
-    {
-        Task PrepareFiltersAndHandleMessages();
-        Task CloseSubscriptionAsync();
-        ValueTask DisposeAsync();
-
-    }
 
     public class ServiceBusTopicSubscription : IServiceBusTopicSubscription
     {
