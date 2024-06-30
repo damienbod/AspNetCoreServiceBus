92341cfcb866cc06f735a8bf06fc17d5bb995e82
damienbod <damien_bod@hotmail.com>
2024-06-30 08:56:34 +0200

    namepaces

--- AspNetCoreServiceBusApi1/Startup.cs
+++ AspNetCoreServiceBusApi1/Startup.cs
@@ -1,63 +1,62 @@
 using Microsoft.OpenApi.Models;
 using ServiceBusMessaging;
 
-namespace AspNetCoreServiceBusApi1
+namespace AspNetCoreServiceBusApi1;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddScoped<ServiceBusSender>();
+        services.AddScoped<ServiceBusTopicSender>();
+
+        services.AddSwaggerGen(c =>
         {
-            Configuration = configuration;
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Payload View API",
+            });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
 
-        public void ConfigureServices(IServiceCollection services)
+        app.UseRouting();
+
+        app.UseAuthorization();
+        app.UseCors();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddControllers();
+            endpoints.MapControllers();
+        });
 
-            services.AddScoped<ServiceBusSender>();
-            services.AddScoped<ServiceBusTopicSender>();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Payload View API",
-                });
-            });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            app.UseCors();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Payload Management API V1");
-            });
-        }
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Payload Management API V1");
+        });
     }
 }
--- FunctionService3/SubscriptionClientFunction.cs
+++ FunctionService3/SubscriptionClientFunction.cs
@@ -2,19 +2,18 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace FunctionService3
-{
-    public static class SubscriptionClientFunction
-    {
-        [FunctionName("SubscriptionClientFunction")]
-        public static void Run([ServiceBusTrigger(
-            "mytopic",
-            "functionsubscription",
-            Connection = "ServiceBusConnectionString")]string message, ILogger log)
-        {
-            log.LogInformation($"C# ServiceBus topic trigger function processed message: {message}");
+namespace FunctionService3;
 
-            var payload = JsonConvert.DeserializeObject<MyPayload>(message);
-        }
+public static class SubscriptionClientFunction
+{
+    [FunctionName("SubscriptionClientFunction")]
+    public static void Run([ServiceBusTrigger(
+        "mytopic",
+        "functionsubscription",
+        Connection = "ServiceBusConnectionString")]string message, ILogger log)
+    {
+        log.LogInformation($"C# ServiceBus topic trigger function processed message: {message}");
+
+        var payload = JsonConvert.DeserializeObject<MyPayload>(message);
     }
 }
